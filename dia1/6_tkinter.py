a72dfc3d7e5662b2f4227046a3678247b2eb9a98
alfredo minchez <aminchez@uni.edu.pe>
2025-12-13 18:01:27 -0500

    new class

---
+++ dia1/6_tkinter.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+#build a new class
+app = Tk()
+
+#title window
+app.title("my first window")
+#size of window
+app.geometry("300x400")
+
+#show this window
+app.mainloop()
